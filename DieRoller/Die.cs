92210b3a3970afc323329d5740f7611f7170bbac
Jaein Lewis <dlwodls0115@gmail.com>
2022-01-13 17:02:03 -0800

    Added constructor, made setting face value private

--- DieRoller/Die.cs
+++ DieRoller/Die.cs
@@ -12,9 +12,17 @@ namespace DieRoller
     public class Die
     {
         /// <summary>
+        /// Create sthe die and rolls it to start with
+        /// a random number1
+        /// </summary>
+        public Die()
+        {
+            Roll();
+        }
+        /// <summary>
         /// The current face up value of the die
         /// </summary>
-        public byte FaceValue { get; set; }
+        public byte FaceValue { get; private set; }
         
         /// <summary>
         /// True if the die is currently held
@@ -29,7 +37,7 @@ namespace DieRoller
         public byte Roll()
         {
             // Generate random number
-            Random random = new Random();
+            Random random = new();
             byte newValue = (byte)random.Next(1, 7);
 
             FaceValue = newValue;
